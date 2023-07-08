b56c4e46799fb0eadfe64ae82040559dd29ebcd3
Alex A <alex.andsten@gmail.com>
2023-07-08 17:19:53 +0200

    styling users

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
-
   <main>
     <div class="users">
+      <img src="src\assets\vendre.png" alt="logo" width="155" height="55">
       <button @click="startVendreUsers">Page 1</button>
       <button @click="startVendreUsers2">Page 2</button>
     </div>
-    <component v-if="showVendreUsers" :is="VendreUsers" api="https://reqres.in/api/users" />
-    <component v-if="showVendreUsers2" :is="VendreUsers" api="https://reqres.in/api/users?page=2" />
-
+      <component v-if="showVendreUsers" :is="VendreUsers" api="https://reqres.in/api/users" />
+      <component v-if="showVendreUsers2" :is="VendreUsers" api="https://reqres.in/api/users?page=2" />
   </main>
 </template>
 
@@ -24,18 +23,20 @@ const showVendreUsers2 = ref(false);
 const startVendreUsers = () => {
   showVendreUsers2.value = false;
   showVendreUsers.value = true;
-  
 };
+
 const startVendreUsers2 = () => {
   showVendreUsers.value = false;
   showVendreUsers2.value = true;
-  
 };
 </script>
 
 
 <style scoped>
 
+main {
+    background-color: rgb(248, 246, 237);
+}
 
 .users {
   display: flex; 
@@ -44,16 +45,31 @@ const startVendreUsers2 = () => {
   margin-right: 7%;
 }
 
-.users button {
-  width: 120px;
-  height: 30px;
+.users img {
+  margin-right: 20%;
 }
 
+.users button {
+  font-family: sans-serif;
+  width: 120px;
+  height: 30px;
+  background-color: rgb(244, 244, 244);
+  border: 3px solid rgb(219, 219, 219);
+  border-radius: 15px;
+  box-shadow: 3px 3px  rgb(141, 141, 141);
+  margin: 3px;
+}
+
+.users button:hover {
+  color: white;
+  background-color: rgb(65, 65, 65);
+  border: 3px solid rgb(241, 240, 240);
+}
 
 @media (max-width: 640px) {
   .users button {
-  width: 80px;
-  height: 20px;
-}
+  width: 100px;
+  height: 25px;
+  }
 }
 </style>
